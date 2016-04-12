795b7ef0fd3f4d7c0cb66969ff721a7e55b23c08
yuqi0822 <victorzhang0822@gmail.com>
2016-04-11 21:36:37 -0400

    Added files via upload

--- src/tool/utils.cpp
+++ src/tool/utils.cpp
@@ -7,18 +7,16 @@ Utils& Utils::getInstance(std::string file){
 
 Utils::Utils(std::string file): fr(fileIO::getIntance()), fileName(file) {
     fr.setNameR(fileName);
+    labelNum = atoi(fr.readLine().c_str());
     tMax = atoi(fr.readLine().c_str());
     cat_num = atoi(fr.readLine().c_str());
     pro_num = atoi(fr.readLine().c_str());
     vec.reserve(cat_num+pro_num);
-
     std::string temp = fr.readLine();
     while( !temp.empty() ) {
         vec.push_back(temp);
         temp = fr.readLine();
     }
-
-
     for(unsigned int i = 0; i < vec.size(); i++) {
          mapping[vec[i]] = i;
         // cout << vec[i] << ": "<< mapping[vec[i]] << endl;
@@ -32,12 +30,9 @@ unordered_map<std::string, int> Utils::getTMap(){
     temp["2016"] = 3;
     return temp;
 }
-
 vector<std::string> Utils::getVector() const {
-
     return vec;
 }
-
 unordered_map<std::string, int> Utils::getMapping() {
 	//cout <<"================"<< mapping["2:2"] << endl;
     return mapping;
--- src/tool/utils.h
+++ src/tool/utils.h
@@ -11,14 +11,17 @@ public:
     vector<std::string> getVector() const;
     unordered_map<std::string, int> getTMap();
     unordered_map<std::string, int> getMapping();
+    int getLabelNum() const {return labelNum;}
     int getCatNum() const { return cat_num; }
     int getProNum() const { return pro_num; }
     int getTMax() const { return tMax; }
+    int getFeatureDim()const {return (cat_num+pro_num)*(tMax);}
 private:
     Utils(std::string file);
     static Utils& instance;
     fileIO& fr;
     std::string fileName;
+    int labelNum;
     int tMax;
     int cat_num;
     int pro_num;
