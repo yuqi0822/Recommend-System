d68a027af2b2fdb8f66fbb661e4424224dd703f3
yuqi0822 <victorzhang0822@gmail.com>
2016-04-11 21:25:42 -0400

    Added files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 2016-4-3
+ *      Author: Yuqi Zhang
+ */
+#include "NaiveBayes.h"
+#include "tool/utils.h"
+
+int main(){
+	string trainFile="trainning.csv",testFile="test.csv",resultFile="output.csv";
+	string mapFileName="index.csv";
+	Utils::getInstance(mapFileName);
+	double thredhold=0.8;
+	NaiveBayes nb(trainFile,testFile,resultFile,thredhold);
+	nb.predict_Similarity();
+	return 0;
+}
+
+
